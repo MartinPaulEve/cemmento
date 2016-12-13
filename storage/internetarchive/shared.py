8f6b436b53046875b9c419c1be4eb217f0035351
Martin Eve <martin@martineve.com>
2016-12-13 13:15:07 -0500

    Add comments to code

--- storage/internetarchive/shared.py
+++ storage/internetarchive/shared.py
@@ -2,4 +2,9 @@ import requests
 
 
 def make_request(url):
+    """
+    Make a request to a remote location
+    :param url: The URL to fetch
+    :return: A response object
+    """
     return requests.request('GET', url)
